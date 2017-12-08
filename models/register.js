4c33d738f76fa32d5d77cfb2643f48841f4bebd2
정준석 <joon123suk@gmail.com>
2017-12-08 19:16:37 +0900

    파일명 변경/정리

---
+++ models/register.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose'),
+    mongoosePaginate = require('mongoose-paginate'),
+    Schema = mongoose.Schema;
+
+var schema = new Schema({
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  event: { type: Schema.Types.ObjectId, ref: 'Event' },
+  content: {type: String, trim: true, required: true},
+  numLikes: {type: Number, default: 0},
+  createdAt: {type: Date, default: Date.now}
+}, {
+  toJSON: { virtuals: true},
+  toObject: {virtuals: true}
+});
+schema.plugin(mongoosePaginate);
+var Register = mongoose.model('Register', schema);
+
+module.exports = Register;
